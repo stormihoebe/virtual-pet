1540bcf760f88155afecece54af9d39178a32460
Stormi Hoebelheinrich <Stormi Hoebelheinrich>
2017-04-03 14:50:56 -0700

    added Monster and Person Class with Tests and Database Rules

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,9 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
 import org.sql2o.*;
 
 public class Person {
+  private String name;
+  private String email;
+  private int id;
 
-  public Person() {
+  public Person(String name, String email) {
+    this.name = name;
+    this.email = email;
+  }
 
+  public String getName() {
+    return name;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  @Override
+  public boolean equals(Object otherPerson){
+    if (!(otherPerson instanceof Person)) {
+      return false;
+    } else {
+      Person newPerson = (Person) otherPerson;
+      return this.getName().equals(newPerson.getName()) &&
+             this.getEmail().equals(newPerson.getEmail());
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO persons (name, email) VALUES (:name, :email)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("email", this.email)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static List<Person> all() {
+    String sql = "SELECT id, name, email FROM persons";
+    try(Connection con = DB.sql2o.open()) {
+     return con.createQuery(sql).executeAndFetch(Person.class);
+    }
+  }
+
+  public List<Monster> getMonsters() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM monsters where personId=:id";
+      return con.createQuery(sql)
+        .addParameter("id", this.id)
+        .executeAndFetch(Monster.class);
+    }
   }
 
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,20 +1,73 @@
-import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
+import org.sql2o.*;
+import java.util.Arrays;
 
 public class PersonTest {
 
-  @Before
-  public void setUp() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/virtual_pets_test", null, null);
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void person_instantiatesCorrectly_true() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    assertEquals(true, testPerson instanceof Person);
   }
 
-  @After
-  public void tearDown() {
-    try (Connection con = DB.sql2o.open()) {
-      String sql = "DELETE FROM name_of_your_table *;";
-      con.createQuery(sql).executeUpdate();
-    }
+  @Test
+  public void getName_personInstantiatesWithName_Henry() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    assertEquals("Henry", testPerson.getName());
+  }
+
+  @Test
+  public void getName_personInstantiatesWithEmail_String() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    assertEquals("henry@henry.com", testPerson.getEmail());
+  }
+
+  @Test
+  public void equals_returnsTrueIfNameAndEmailAreSame_true() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    Person anotherPerson = new Person("Henry", "henry@henry.com");
+    assertTrue(testPerson.equals(anotherPerson));
+  }
+
+  @Test
+  public void save_insertsObjectIntoDatabase_Person() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    assertEquals(true, Person.all().get(0).equals(testPerson));
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfPerson_true() {
+    Person firstPerson = new Person("Henry", "henry@henry.com");
+    firstPerson.save();
+    Person secondPerson = new Person("Harriet", "harriet@harriet.com");
+    secondPerson.save();
+    assertEquals(true, Person.all().get(0).equals(firstPerson));
+    assertEquals(true, Person.all().get(1).equals(secondPerson));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    Person savedPerson = Person.all().get(0);
+    assertEquals(testPerson.getId(), savedPerson.getId());
+  }
+
+  @Test
+  public void getMonsters_retrievesAllMonstersFromDatabase_monstersList() {
+    Person testPerson = new Person("Henry", "henry@henry.com");
+    testPerson.save();
+    Monster firstMonster = new Monster("Bubbles", testPerson.getId());
+    firstMonster.save();
+    Monster secondMonster = new Monster("Spud", testPerson.getId());
+    secondMonster.save();
+    Monster[] monsters = new Monster[] { firstMonster, secondMonster };
+    assertTrue(testPerson.getMonsters().containsAll(Arrays.asList(monsters)));
   }
 
 }
